00b1067175a48f4ea93355bd02acf1100fe3e1a7
andrewwhite5 <andrewwhite5@users.noreply.github.com>
2020-10-14 20:11:34 -0600

    Basic structure

--- dbscan.py
+++ dbscan.py
@@ -6,3 +6,31 @@ https://github.com/scikit-learn/scikit-learn/blob/0fb307bf3/sklearn/cluster/_dbs
 '''
 
 import numpy as np
+
+def dbscan():
+    '''
+    TODO:
+        - Figure out callable parameters ^
+        -
+    '''
+    # estimate = DBSCAN(parameters here)
+    # estimate.fit
+    pass
+
+class DBSCAN():
+    '''
+    TODO:
+        - Figure out callable parameters
+        - Define fit() method
+        - Define predict() method
+    '''
+
+    def __init__(self, dist=.1, min_samples=3):
+        self.dist = dist
+        self.min_samples = min_samples
+
+    def fit():
+        pass
+
+    def predict():
+        pass
